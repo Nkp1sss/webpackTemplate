ec268e893448a39aad0eed780c714069dcaa6d57
Nikita <nikitakemper555@gmail.com>
2022-12-02 01:31:30 +0300

    feat: add ts eslint setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { doSomething } from './scripts/module';
 import './styles/styles.scss';
 import './styles/normalize.css';
+import { sum } from './scripts/test';
+
+console.log(sum(1, 2))
 
 doSomething();
 
